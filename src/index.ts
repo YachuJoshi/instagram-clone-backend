732c877023cf9640b7357fffeba60508fbbf3faa
Yachu Joshi <yachujoshi1234@gmail.com>
2022-04-30 10:40:14 +0545

    Minor setup changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", (req: Request, res: Response) => {
   return res.json({ message: "Hello World!" });
 });
 
